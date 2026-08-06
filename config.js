a11fc282ca20f0a01c41680cdec0baa8fbb5c9a4
homeyo <__>
2026-08-06 12:46:21 +0900

    사용하지 않는 ADMIN_PW 제거

--- config.js
+++ config.js
@@ -3,5 +3,3 @@ const SB_URL = 'https://gaigimhrlyvosgbwauxi.supabase.co';
 const SB_KEY = 'sb_publishable_XdNI8L7FS52uhLvt2xSW1Q_OGtxfn9S';
 const sb = supabase.createClient(SB_URL, SB_KEY);
 
-// ===== 관리자 페이지 비밀번호 =====
-const ADMIN_PW = '111111';
